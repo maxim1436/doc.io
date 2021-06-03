06d038e668b080dd302cf9bf367a974da02350a0
maxim1436 <36988345+maxim1436@users.noreply.github.com>
2021-06-03 15:17:10 +1000

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+/*!
+     \brief Функция, которая выполняет сложенние двух переменных.
+
+            Данная функция возвращает сумму двух переменных x и y, тип возвращаемого значения int.
+            В качестве параметров, в функцию передаются два числа типа int.
+*/
+int f (int x, int y)
+{
+return (x+y);
+}
+
+int main()
+{
+    std::cout<<"Hello World"<<endl;
+    std::cout<<"I learn Git"<<endl;
+    std::cout<<"Silence is gold"<<endl;
+    std::cout<<"Welcome home!"<<endl;
+    std::cout<<"Print error!"<<endl;
+
+int1 a = 53, b = 10;
+std::cout << "hello world!";
+
+std::cout<<f(a, b)<<endl;
+
+return 0
+}
